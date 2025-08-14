93ae80e19f0c88143fc488601aadf9da8098a3c3
Tristan Henning <tristan@customcrypto.com>
2025-08-14 01:00:33 -0500

    Cleared out more models and simplified user, bug exists

--- main.v
+++ main.v
@@ -22,22 +22,9 @@ fn main() {
 
     sql app.db {
         create table User
-        create table ApiUser
     } or { panic(error) }
 
     veb.run[App, Context](mut app, app.port)
-}
-
-enum Permission as u8 {
-    read_only
-    user
-    admin
-    owner
-}
-
-enum UserStatus as u8 {
-    enabled
-    disabled
 }
 
 enum HashAlgorithm as u8 {
@@ -55,41 +42,18 @@ pub mut:
 pub struct User {
 pub:
     user_id         int         @[primary; unique; serial]
-    status          UserStatus
-    name            string
-    email           string
     password_hash   PasswordHash
-    mfa_token       ?string
-    permisisons     Permission
-    api_keys        ?[]ApiUser   @[fkey: 'creator_user_id']
-}
-
-@[table: 'ApiUsers']
-pub struct ApiUser {
-pub:
-    api_key_id      int         @[primary; unique]
-    creator_user_id int
-    description     string
-    status          UserStatus
-    creation        i64
-    expiration      i64
-    permissions     Permission
-    api_key         string
 }
 
 @['/api/user_registration'; get; post]
 pub fn (app &App) user_registration(mut ctx Context) veb.Result {
 
 	new_user := User {
-		status: UserStatus.enabled
-		name: 'testu'
-		email: 'teste'
 		password_hash: PasswordHash{
 			algorithm:	HashAlgorithm.sha256
 			hash:		'1234'
 			salt:		'1234'
 		}
-		permisisons: Permission.owner
 	}
 
 	// Compiler Bug
